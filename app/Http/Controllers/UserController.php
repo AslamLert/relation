518b3d81b02a6e0f3b37c9214ec54a97c787d080
Aslam <gloommyday@gmail.com>
2021-08-24 21:02:52 +0700

    1to1 Controller Route-Add

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Phone;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function insertRecord()
+    {
+    $phone = new Phone();
+    $phone->phone = "1234567890";
+    $user = new User();
+    $user->name = "aa";
+    $user->email = "aa@aa.com";
+    $user->password = encrypt('aaaaaaaa');
+    $user->save();
+    $user->phone()->save($phone);
+    return "Record has been created success";
+
+    }
+
+}
